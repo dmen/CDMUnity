34a5d425aa12683c8c8ede5d37a2d33da1b71c6b
MARKETING\dmennenoh <dmennenoh@gmrmarketing.com>
2018-02-13 09:21:35 -0600

    kt

--- Assets/Scripts/ExitNormManager.cs
+++ Assets/Scripts/ExitNormManager.cs
@@ -13,8 +13,6 @@ public class ExitNormManager : MonoBehaviour
     private CanvasGroup startButton;
     private GameObject man;
 
-    private Material vidMat;
-
 
     void Start()
     {
@@ -25,41 +23,11 @@ public class ExitNormManager : MonoBehaviour
         mainText = GameObject.Find("mainText").GetComponent<CanvasGroup>();
 
         startButton = GameObject.Find("buttonExit").GetComponent<CanvasGroup>();
-      
+
         logo.alpha = 0;
         mainText.alpha = 0;
         startButton.alpha = 0;
 
-        vidMat = GameObject.Find("vidShow").GetComponent<Image>().material;
-        vidMat.color = new Color(1, 1, 1, 0);
-
-        LeanTween.delayedCall(1f, startVideo);
-    }
-
-
-    void startVideo()
-    {
-        GameObject.Find("introVideo").GetComponent<VideoPlayer>().Play();
-        LeanTween.delayedCall(.25f, moveVideoDown);
-        LeanTween.delayedCall(3f, fadeOutVideo);
-    }
-
-
-    void moveVideoDown()
-    {
-        LeanTween.value(man, setVidColor, new Color(1, 1, 1, 0), new Color(1, 1, 1, 1), .5f);
-        GameObject.Find("vidShow").GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
-    }
-
-
-    void fadeOutVideo()
-    {
-        LeanTween.value(man, setVidColor, new Color(1, 1, 1, 1), new Color(1, 1, 1, 0), 1f).setOnComplete(showThings);
-    }
-
-
-    void showThings()
-    {
         LeanTween.alphaCanvas(logo, 1f, 2f);
         LeanTween.alphaCanvas(mainText, 1f, 1f).setDelay(.5f);
 
@@ -71,10 +39,6 @@ public class ExitNormManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);//vr/norm selector
     }
-
-    void setVidColor(Color val)
-    {
-        vidMat.color = val;
-    }
+    
 
 }
